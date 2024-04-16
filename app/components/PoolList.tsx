2605bce28328a5e547a2a38be8b31e9e4e974e5d
Purin Buriwong <purin.b@bluepi.co.th>
2024-04-16 19:57:14 +0700

    Keep data in session storage to prevent multiple api calls

--- app/components/PoolList.tsx
+++ app/components/PoolList.tsx
@@ -1,32 +1,33 @@
 'use client';
 import MempoolService from "@Services/MempoolService";
 import { useEffect, useState } from "react";
-import { PoolData, PoolListResponse } from "@Models/services/MempoolServicesModel";
+import useStore from "@Hooks/useStore";
 
 const PoolList = () => {
-const [data, setData] = useState<PoolListResponse[] | null>(null);
+const { data, setWeeklyTop } = useStore();
 
-  useEffect(() => {
-    const fetchTopTenPools = async () => {
-      const topTen = await MempoolService.getTopTenPools();
-      setData(topTen);
-    };
-    
-    fetchTopTenPools();
-  }, []);
+useEffect(() => {
+  const fetchTopTenPools = async () => {
+    const topTen = await MempoolService.getTopTenPools();
+    topTen && setWeeklyTop(topTen);
+  };
+  data === null && fetchTopTenPools();
+}, [data, setWeeklyTop]);
+
   return (
     <div>
       <h1>Pool List</h1>
-      {/* <h1>{data && data.blockCount}</h1> */}
-      {data && data.map((pool, index) => (
-        <div key={index}>
-          <p>rank:{pool.rank}</p>
-          <h2>{pool.name}</h2>
-          <p>Link: <a href={pool.link}>{pool.link}</a></p>
-          <p>Block Count: {pool.blockCount}</p>
-          <hr />
-        </div>
-      ))}
+      {data ?  (
+        data.map((pool, index) => (
+          <div key={index}>
+            <p>rank:{pool.rank}</p>
+            <h2>{pool.name}</h2>
+            <p>Link: <a href={pool.link}>{pool.link}</a></p>
+            <p>Block Count: {pool.blockCount}</p>
+            <hr />
+          </div>
+       ))
+      ) : ( <p>Loading...</p>)}
     </div>
   );
 };
---
+++ app/hooks/useStore.ts
@@ -0,0 +1,35 @@
+'use client';
+import { create } from 'zustand';
+import { PoolListResponse } from "@Models/services/MempoolServicesModel";
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+interface PoolStore {
+  data: PoolListResponse[] | null;
+  setWeeklyTop: (data: PoolListResponse[] | null) => void;
+}
+
+const initialState = () => {
+  // ensure that this code is only executed on the client
+  if (typeof window !== 'undefined') {
+    const saved = sessionStorage.getItem('pool-storage');
+    console.log('clientside now')
+    return saved ? JSON.parse(saved).data : null;
+  }
+  return null;
+};
+
+const useStore = create<PoolStore>()(
+  persist(
+    (set) => ({
+        data: initialState(),
+        setWeeklyTop: (data: PoolListResponse[] | null) => {
+          set({ data });
+        }
+      }),
+    {
+      name: 'pool-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
+export default useStore;
